f4bd6fe8eaea98bd9906c36b606dbcc70d06813b
Jihong Park <ggikko@hotmail.com>
2016-06-01 10:51:33 +0900

    changed

--- ButterknifeTApp/app/src/main/java/ggikko/me/butterknifetapp/MainActivity.java
+++ ButterknifeTApp/app/src/main/java/ggikko/me/butterknifetapp/MainActivity.java
@@ -1,10 +1,16 @@
 package ggikko.me.butterknifetapp;
 
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
+import butterknife.BindColor;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 //public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 public class MainActivity extends AppCompatActivity  {
@@ -12,12 +18,63 @@ public class MainActivity extends AppCompatActivity  {
 //    TextView mText1, mText2 ,mText3, mText4, mText5;
 //    Button mButton1, mButton2 ,mButton3, mButton4, mButton5;
 
+    @BindView(R.id.text1) TextView text1;
+    @BindView(R.id.text2) TextView text2;
+    @BindView(R.id.text3) TextView text3;
+    @BindView(R.id.text4) TextView text4;
+    @BindView(R.id.text5) TextView text5;
+
+    @BindColor(R.color.blue) int blue;
+
+
+    @OnClick({R.id.btn_text1, R.id.btn_text2, R.id.btn_text3, R.id.btn_text4, R.id.btn_text5})
+    void pressButtons(View view){
+        switch (view.getId()){
+
+            case R.id.btn_text1 :{
+                setText(text1, text2, text3, text4, text5);
+
+//                text1.setText("onclick!");
+                break;
+            }
+
+            case R.id.btn_text2 :{
+                text2.setText("onclick!");
+                break;
+            }
+
+            case R.id.btn_text3 :{
+                text3.setText("onclick!");
+                break;
+            }
+
+            case R.id.btn_text4 :{
+                text4.setText("onclick!");
+                break;
+            }
+
+            case R.id.btn_text5 :{
+                text5.setText("onclick!");
+                break;
+            }
+        }
+    }
+
+    private void setText(TextView... textViews){
+        for(TextView textView : textViews) {
+            textView.setText("onclick!");
+//            textView.setTextColor(ContextCompat.getColor(this, R.color.blue));
+            textView.setTextColor(blue);
+        }
+    }
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(this);
 
 //        mText1 = (TextView) findViewById(R.id.text1);
 //        mText2 = (TextView) findViewById(R.id.text2);
