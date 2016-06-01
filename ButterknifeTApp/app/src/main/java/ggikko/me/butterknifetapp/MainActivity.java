3ab51af99b95ea13ddb5ab54e5f864a6f7c89a4a
Jihong Park <ggikko@hotmail.com>
2016-06-01 10:35:09 +0900

    legacy

--- ButterknifeTApp/app/src/main/java/ggikko/me/butterknifetapp/MainActivity.java
+++ ButterknifeTApp/app/src/main/java/ggikko/me/butterknifetapp/MainActivity.java
@@ -2,12 +2,84 @@ package ggikko.me.butterknifetapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+//public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity  {
+
+//    TextView mText1, mText2 ,mText3, mText4, mText5;
+//    Button mButton1, mButton2 ,mButton3, mButton4, mButton5;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//        mText1 = (TextView) findViewById(R.id.text1);
+//        mText2 = (TextView) findViewById(R.id.text2);
+//        mText3 = (TextView) findViewById(R.id.text3);
+//        mText4 = (TextView) findViewById(R.id.text4);
+//        mText5 = (TextView) findViewById(R.id.text5);
+//
+//        mButton1 = (Button) findViewById(R.id.btn_text1);
+//        mButton2 = (Button) findViewById(R.id.btn_text2);
+//        mButton3 = (Button) findViewById(R.id.btn_text3);
+//        mButton4 = (Button) findViewById(R.id.btn_text4);
+//        mButton5 = (Button) findViewById(R.id.btn_text5);
+//
+//        mButton1.setOnClickListener(this);
+//
+//        mButton1.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                mText1.setText("one click!");
+//            }
+//        });
+//
+//        mButton2.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                mText2.setText("one click!");
+//            }
+//        });
+//
+//
+//        mButton3.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                mText3.setText("one click!");
+//            }
+//        });
+//
+//
+//        mButton4.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                mText4.setText("one click!");
+//            }
+//        });
+//
+//        mButton5.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                mText5.setText("one click!");
+//            }
+//        });
     }
+
+//    @Override
+//    public void onClick(View v) {
+//        switch (v.getId()) {
+//            case R.id.btn_text1:
+//                break;
+//
+//
+//        }
+//    }
+
+
 }
